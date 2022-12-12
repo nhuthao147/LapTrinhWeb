08d3d906b1b967ec3d708b636ca9312ea696277a
duynn01092002 <nguyenngocduy12a2@gmail.com>
2022-12-12 11:08:06 +0700

    Update ProductServiceImpl.java

--- src/main/java/com/edu/laptrinhweb/nhom4/service/impl/ProductServiceImpl.java
+++ src/main/java/com/edu/laptrinhweb/nhom4/service/impl/ProductServiceImpl.java
@@ -4,23 +4,23 @@ import com.edu.laptrinhweb.nhom4.model.Product;
 import com.edu.laptrinhweb.nhom4.repository.ProductRepository;
 import com.edu.laptrinhweb.nhom4.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Random;
 
 @Component
 public class ProductServiceImpl implements ProductService{
+    private static Random rnd = new Random();
     @Autowired
     ProductRepository productRepository;
 
     @Override
 	public List<Product> getAllProduct() {
-
-        for (Product p: productRepository.findAll()
-        ) {
-            System.out.println(p.getName()+" - "+p.getImageName());
-        }return productRepository.findAll();
+        return productRepository.findAll();
     }//findAll
 
     @Override
@@ -43,5 +43,40 @@ public class ProductServiceImpl implements ProductService{
         return productRepository.findAllByCategory_Id(id);
     }
     //findList theo ProductDTO.categoryId
+
+    @Override
+    public Page<Product> findByProductNameContaining(String name, Pageable pageable) {
+        return productRepository.findByNameContaining(name, pageable);
+    }
+
+    @Override
+    public List<Product> findByProductNameContaining(String name) {
+        return productRepository.findByNameContaining(name);
+    }
+
+    @Override
+    public Page<Product> findAll(Pageable pageable) {
+        return productRepository.findAll(pageable);
+    }
+
+    @Override
+    public long count() {
+        return productRepository.count();
+    }
+
+    @Override
+    public List<Product> getRandomListProducts(int lenght) {
+        List<Product> productList = productRepository.findAll();
+        int lastIndex;
+        lastIndex = productList.size() -1;
+        while(lastIndex > 0 ){
+            Product tempValue = productList.get(lastIndex);
+            int randomIndex = rnd.nextInt(lastIndex);
+            productList.set(lastIndex, productList.get(randomIndex));
+            productList.set(randomIndex, tempValue);
+            lastIndex--;
+        }
+        return productList.subList(0, lenght);
+    }
 
 }
