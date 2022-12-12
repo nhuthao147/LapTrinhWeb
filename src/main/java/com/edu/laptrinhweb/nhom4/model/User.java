bb4a506069efe7cddffed72473de1e4fdf81b26c
duynn01092002 <nguyenngocduy12a2@gmail.com>
2022-12-12 11:02:45 +0700

    Update User.java

--- src/main/java/com/edu/laptrinhweb/nhom4/model/User.java
+++ src/main/java/com/edu/laptrinhweb/nhom4/model/User.java
@@ -47,4 +47,53 @@ public class User {
         this.roles = user.getRoles();
     }
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+    
+
 }//create table mapping trong db
