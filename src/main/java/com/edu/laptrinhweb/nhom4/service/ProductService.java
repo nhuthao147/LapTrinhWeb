2b9df3fa6723f46ae0162906d874bff3162365e1
duynn01092002 <nguyenngocduy12a2@gmail.com>
2022-12-12 11:08:10 +0700

    Update ProductService.java

--- src/main/java/com/edu/laptrinhweb/nhom4/service/ProductService.java
+++ src/main/java/com/edu/laptrinhweb/nhom4/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.edu.laptrinhweb.nhom4.service;
 
 import com.edu.laptrinhweb.nhom4.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -18,5 +20,13 @@ public interface ProductService {
 	void updateProduct(Product product);
 
 	List<Product> getAllProduct();
-    
+
+	Page<Product> findByProductNameContaining(String name, Pageable pageable);
+
+	List<Product> findByProductNameContaining(String name);
+
+	Page<Product> findAll(Pageable pageable);
+
+	long count();
+	List<Product> getRandomListProducts(int lenght);
 }
