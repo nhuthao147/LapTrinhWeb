62c58ae75594b6cccd8e0efe54e98286b7fadcc1
duynn01092002 <nguyenngocduy12a2@gmail.com>
2022-12-12 11:08:08 +0700

    Update UserServiceImpl.java

--- src/main/java/com/edu/laptrinhweb/nhom4/service/impl/UserServiceImpl.java
+++ src/main/java/com/edu/laptrinhweb/nhom4/service/impl/UserServiceImpl.java
@@ -4,14 +4,26 @@ import com.edu.laptrinhweb.nhom4.model.User;
 import com.edu.laptrinhweb.nhom4.repository.UserRepository;
 import com.edu.laptrinhweb.nhom4.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
+import java.awt.print.Pageable;
 import java.util.List;
 import java.util.Optional;
 @Component
 public class UserServiceImpl implements UserService {
     @Autowired
     UserRepository userRepository;
+
+    public Page<User> findAll(org.springframework.data.domain.Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    @Override
+    public long count() {
+        return userRepository.count();
+    }
+
     @Override
     public List<User> getAllUser() {
         return userRepository.findAll();
