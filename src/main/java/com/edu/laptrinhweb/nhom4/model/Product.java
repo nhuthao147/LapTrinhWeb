219f1b09b85b816782856d493cdf146c8879be21
duynn01092002 <nguyenngocduy12a2@gmail.com>
2022-12-12 11:02:39 +0700

    Update Product.java

--- src/main/java/com/edu/laptrinhweb/nhom4/model/Product.java
+++ src/main/java/com/edu/laptrinhweb/nhom4/model/Product.java
@@ -25,4 +25,60 @@ public class Product {
 
     private String imageName;
 
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
+	public void setCategory(Category category) {
+		this.category = category;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getImageName() {
+		return imageName;
+	}
+
+	public void setImageName(String imageName) {
+		this.imageName = imageName;
+	}
+
 }//create table mapping trong db
