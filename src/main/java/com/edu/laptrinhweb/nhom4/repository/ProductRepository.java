eed404e6260c48173a63226186f3a71e98803e9d
duynn01092002 <nguyenngocduy12a2@gmail.com>
2022-12-12 11:07:52 +0700

    Update ProductRepository.java

--- src/main/java/com/edu/laptrinhweb/nhom4/repository/ProductRepository.java
+++ src/main/java/com/edu/laptrinhweb/nhom4/repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.edu.laptrinhweb.nhom4.repository;
 
 import com.edu.laptrinhweb.nhom4.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,7 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     List<Product> findAllByCategory_Id(int id);
+    Page<Product> findByNameContaining(String name, Pageable pageable);
 
+    List<Product> findByNameContaining(String name);
 }
