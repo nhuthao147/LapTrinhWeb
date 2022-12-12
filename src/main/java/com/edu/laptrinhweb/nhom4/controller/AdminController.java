e1aa357527cc1abe551e013eb34888939c34898f
duynn01092002 <nguyenngocduy12a2@gmail.com>
2022-12-12 11:07:30 +0700

    Update AdminController.java

--- src/main/java/com/edu/laptrinhweb/nhom4/controller/AdminController.java
+++ src/main/java/com/edu/laptrinhweb/nhom4/controller/AdminController.java
@@ -2,18 +2,20 @@ package com.edu.laptrinhweb.nhom4.controller;
 
 import com.edu.laptrinhweb.nhom4.dto.ProductDTO;
 import com.edu.laptrinhweb.nhom4.dto.UserDTO;
-import com.edu.laptrinhweb.nhom4.model.Category;
-import com.edu.laptrinhweb.nhom4.model.Product;
-import com.edu.laptrinhweb.nhom4.model.Role;
-import com.edu.laptrinhweb.nhom4.model.User;
-import com.edu.laptrinhweb.nhom4.service.CategoryService;
-import com.edu.laptrinhweb.nhom4.service.ProductService;
-import com.edu.laptrinhweb.nhom4.service.RoleService;
-import com.edu.laptrinhweb.nhom4.service.UserService;
+import com.edu.laptrinhweb.nhom4.global.GlobalData;
+import com.edu.laptrinhweb.nhom4.model.*;
+import com.edu.laptrinhweb.nhom4.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -24,6 +26,8 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class AdminController {
@@ -43,6 +47,8 @@ public class AdminController {
     @Autowired
     RoleService roleService;
 
+    @Autowired
+    BillService billService;
     @GetMapping("/admin")
     public String adminHome(){
         return "adminHome";
@@ -50,8 +56,34 @@ public class AdminController {
 
     //Accounts
     @GetMapping("/admin/users")
-    public String getAcc(Model model){
-        model.addAttribute("users", userService.getAllUser());
+    public String getAcc(Model model,
+                         @RequestParam("page") Optional<Integer> page,
+                         @RequestParam("size") Optional<Integer> size){
+        int count = (int) userService.count();
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(5);
+
+        Pageable pageable = PageRequest.of(currentPage -1, pageSize, Sort.by("id"));
+        Page<User> resultPage = null;
+        resultPage = userService.findAll(pageable);
+
+        int totalPages = resultPage.getTotalPages();
+        if(totalPages > 0) {
+            int start = Math.max(1, currentPage-2);
+            int end = Math.min(currentPage+2, totalPages);
+            if(totalPages > count) {
+                if(end == totalPages) start = end - count;
+                else if(start == 1) end = start + count;
+            }
+            List<Integer> pageNumbers = IntStream.rangeClosed(start, end)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
+        model.addAttribute("userPage", resultPage);
+
+//        model.addAttribute("products", productService.getAllProduct());
         //model.addAttribute("roles", roleService.getAllRole());
         return "users";
     }
@@ -113,8 +145,35 @@ public class AdminController {
 
     //Categories session
     @GetMapping("/admin/categories")
-    public String getCat(Model model){
-        model.addAttribute("categories", categoryService.getAllCategory());
+    public String getCat(Model model,
+                         @RequestParam("page") Optional<Integer> page,
+                         @RequestParam("size") Optional<Integer> size){
+        int count = (int) categoryService.count();
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(5);
+
+        Pageable pageable = PageRequest.of(currentPage -1, pageSize, Sort.by("id"));
+        Page<Category> resultPage = null;
+        resultPage = categoryService.findAll(pageable);
+
+        int totalPages = resultPage.getTotalPages();
+        if(totalPages > 0) {
+            int start = Math.max(1, currentPage-2);
+            int end = Math.min(currentPage+2, totalPages);
+            if(totalPages > count) {
+                if(end == totalPages) start = end - count;
+                else if(start == 1) end = start + count;
+            }
+            List<Integer> pageNumbers = IntStream.rangeClosed(start, end)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
+        model.addAttribute("categoryPage", resultPage);
+
+//        model.addAttribute("products", productService.getAllProduct());
+        //model.addAttribute("roles", roleService.getAllRole());
         return "categories";
     }//view all categories
 
@@ -148,11 +207,45 @@ public class AdminController {
     }//form edit category, fill old data into form
 
     //Products session
-    @GetMapping("/admin/products")
-    public String getPro(Model model){
-        model.addAttribute("products", productService.getAllProduct());
+    @RequestMapping("/admin/products")
+    public String getPro(Model model,
+                         @RequestParam(name="name",required = false) String name,
+                         @RequestParam("page") Optional<Integer> page,
+                         @RequestParam("size") Optional<Integer> size){
+        int count = (int) productService.count();
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(5);
+
+        Pageable pageable = PageRequest.of(currentPage -1, pageSize, Sort.by("id"));
+        Page<Product> resultPage = null;
+        if(StringUtils.hasText(name)) {
+            resultPage = productService.findByProductNameContaining(name, pageable);
+            model.addAttribute("name", name);
+        }
+        else {
+            resultPage = productService.findAll(pageable);
+        }
+
+        int totalPages = resultPage.getTotalPages();
+        if(totalPages > 0) {
+            int start = Math.max(1, currentPage-2);
+            int end = Math.min(currentPage+2, totalPages);
+            if(totalPages > count) {
+                if(end == totalPages) start = end - count;
+                else if(start == 1) end = start + count;
+            }
+            List<Integer> pageNumbers = IntStream.rangeClosed(start, end)
+                    .boxed()
+                    .collect(Collectors.toList());
+            model.addAttribute("pageNumbers", pageNumbers);
+        }
+
+        model.addAttribute("productPage", resultPage);
+
+//        model.addAttribute("products", productService.getAllProduct());
         return "products";
     }//view all products
+
 
     @GetMapping("/admin/products/add")
     public String getProAdd(Model model){
@@ -215,4 +308,73 @@ public class AdminController {
         }
 
     }//form edit product, fill old data into form
+
+
+
+    //Bill session
+    @GetMapping("/admin/bill")
+    public String getBill(Model model){
+        model.addAttribute("categories", billService.getAllBill());
+        return "checkout";
+    }//view all categories
+
+    @GetMapping("/bill/add")
+    public String getBillAdd(Model model){
+        Bill bill = new Bill();
+        model.addAttribute("cartCount", GlobalData.cart.size());
+        model.addAttribute("total", GlobalData.cart.stream().mapToDouble(Product::getPrice).sum());
+        UserDTO currentUser = new UserDTO();
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof UserDetails && ((UserDetails) principal).getUsername() != null) {
+            String currentUsername = ((UserDetails)principal).getUsername();
+            User user = userService.getUserByEmail(currentUsername).get();
+            currentUser.setId(user.getId());
+            currentUser.setEmail(user.getEmail());
+            currentUser.setPassword("");
+            currentUser.setFirstName(user.getFirstName());
+            currentUser.setLastName(user.getLastName());
+            List<Integer> roleIds = new ArrayList<>();
+            for (Role item:user.getRoles()) {
+                roleIds.add(item.getId());
+            }
+            currentUser.setRoleIds(roleIds);
+
+        }//get current User runtime
+
+        bill.setFirstName(currentUser.getFirstName());
+        bill.setLastName(currentUser.getLastName());
+        bill.setEmail(currentUser.getEmail());
+
+        model.addAttribute("bill", bill);
+        return "checkout";
+    }//form add new category
+
+    @PostMapping("/bill/add")
+    public String postBillAdd(@ModelAttribute("bill") Bill bill){
+        bill.setTotal(GlobalData.cart.stream().mapToDouble(Product::getPrice).sum());
+        bill.setUser(userService.getUserByEmail(bill.getEmail()).get());
+//        Set<Product> productSet = GlobalData.cart.;
+//        List<Product> products = GlobalData.cart;
+//        products.forEach(p->bill.setProducts(p));
+        billService.saveSill(bill, GlobalData.cart);
+        GlobalData.cart = new ArrayList<>();
+        return "redirect:/";
+    }//form add new category > do add
+
+    @GetMapping("/admin/bill/delete/{id}")
+    public String deleteBill(@PathVariable Long id){
+        billService.removeBillById(id);
+        return "redirect:/admin/bill";
+    }//delete 1 category
+
+    @GetMapping("/admin/bill/update/{id}")
+    public String updateBill(@PathVariable Long id, Model model){
+        Optional<Bill> bill = billService.getBillById(id);
+        if(bill.isPresent()){
+            model.addAttribute("bill", bill.get());
+            return "billsAdd";
+        }else {
+            return "404";
+        }
+    }//form edit category, fill old data into form
 }
